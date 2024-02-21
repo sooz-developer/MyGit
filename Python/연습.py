73fa6580e7afe412fe4c7261af7ac9bcf929d1ff
Sooz <soozdeveloper@gmail.com>
2024-02-21 09:35:11 +0900

    0220화요일

---
+++ Python/연습.py
@@ -0,0 +1,9 @@
+list_data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(list_data)
+del list_data[6]
+print(list_data)
+
+country_capital = {"영국":"런던", "프랑스":"파리", "스위스":"베른"}
+print(country_capital["영국"])
+
+
